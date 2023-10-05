cf863c1d19237ec91e4a38dde3ad951a9fff1842
Joseph M. Temesi <joemunyendo@gmail.com>
2023-10-05 14:58:40 +0300

    Added changes

--- game.js
+++ game.js
@@ -1,94 +1,84 @@
-var userClickedPattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
-var buttonColours = ['red', 'blue', 'green', 'yellow'];
-var documentHeaderLevel = 0;
-var hasOperationOccurred = false; //check key press number
+var userClickedPattern = [];
 
-function nextSequence() {
-    userClickedPattern = [];
-    var randomNumber = Math.floor(Math.random() * 4);
-    documentHeaderLevel = documentHeaderLevel + 1;
-    var documentHeader = $("#level-title"); // Use # for ID
-    documentHeader.text("Level " + documentHeaderLevel);
-    return buttonColours[randomNumber];
-}
+var started = false;
+var level = 0;
 
-// var randomChosenColour = nextSequence();
-// gamePattern.push(randomChosenColour);
-
-$(document).keydown(function(event){ //key down event listener
-    if (!hasOperationOccurred){
-        var randomChosenColour = nextSequence();
-    gamePattern.push(randomChosenColour);
-    userClickedPattern.push(randomChosenColour)
-    console.log(gamePattern[gamePattern.length - 1]);
-
-    switch (randomChosenColour) {
-        case "blue":
-            var blue = $("#" + randomChosenColour);
-            animationButton(randomChosenColour);
-            playSound(randomChosenColour);
-            break;
-        case "green":
-            var green = $("#" + randomChosenColour);
-            animationButton(randomChosenColour);
-            playSound(randomChosenColour);
-            break;
-        case "red":
-            var red = $("#" + randomChosenColour);
-            animationButton(randomChosenColour);
-            playSound(randomChosenColour);
-            break;
-        case "yellow":
-            var yellow = $("#" + randomChosenColour);
-            animationButton(randomChosenColour);
-            playSound(randomChosenColour);
-            break; // Add 'break' to exit the 'switch'
-        default:
-            // Code to be executed if expression doesn't match any case
-            break; // Add 'break' for the 'default' case
-    }
-    hasOperationOccurred = true;
-    }
-
-})
-  
-
-
-
-
-function animationButton(pressedKey) {
-    var pressedKeyButton = $("#" + pressedKey);
-    pressedKeyButton.addClass("pressed");
-
-    setTimeout(function () {
-        pressedKeyButton.removeClass("pressed");
-    }, 100);
-}
-
-$(".btn").click(function(){ //handler funxtion store id of clicked button
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
     nextSequence();
-    var userChosenColour = $(this).attr('id');
-    userClickedPattern.push(userChosenColour);
-    var lastElement = userClickedPattern[userClickedPattern.length - 1];
-    playSound(lastElement);
-    checkAnswer(userClickedPattern.length - 1) /// push index of the last answer the last answer.
-
-    console.log(gamePattern[gamePattern.length - 1]); //ok gamepattern
-   console.log(userClickedPattern.length - 1); //ok userclicked pattern
+    started = true;
+  }
 });
 
-function playSound(buttonColor) {
-    var audio = new Audio("sounds/" + buttonColor + ".mp3");
-    audio.play();
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+
+    } else {
+
+      console.log("wrong");
+
+      //1. In the sounds folder, there is a sound called wrong.mp3, play this sound if the user got one of the answers wrong.
+      playSound("wrong");
+
+      //2. In the styles.css file, there is a class called "game-over", apply this class to the body of the website when the user gets one of the answers wrong and then remove it after 200 milliseconds.
+      $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      //3. Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+    }
+
 }
 
-function checkAnswer(currentLevel){ //check if the most recent user answer is the same as the game pattern
-    var gamePattern1 = gamePattern[gamePattern.length-1];
-    var userClickedPattern1 = userClickedPattern[currentLevel];
-    if(userClickedPattern1 == gamePattern1){
-        console.log("success");
-    } else {
-        console.log("fail");
-    }
+function nextSequence() {
+
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
