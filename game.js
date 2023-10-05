127f75170decc9368bb941778213ec0c2973af22
Joseph M. Temesi <joemunyendo@gmail.com>
2023-10-05 12:50:15 +0300

    Added changes

--- game.js
+++ game.js
@@ -1,22 +1,27 @@
 var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ['red', 'blue', 'green', 'yellow'];
+var documentHeaderLevel = 0;
+var hasOperationOccurred = false; //check key press number
 
 function nextSequence() {
+    userClickedPattern = [];
     var randomNumber = Math.floor(Math.random() * 4);
+    documentHeaderLevel = documentHeaderLevel + 1;
+    var documentHeader = $("#level-title"); // Use # for ID
+    documentHeader.text("Level " + documentHeaderLevel);
     return buttonColours[randomNumber];
 }
 
 // var randomChosenColour = nextSequence();
 // gamePattern.push(randomChosenColour);
 
-var hasOperationOccurred = false; //check key press number
-
 $(document).keydown(function(event){ //key down event listener
     if (!hasOperationOccurred){
         var randomChosenColour = nextSequence();
-    gamePattern.push(randomChosenColour); 
-    console.log(randomChosenColour)
+    gamePattern.push(randomChosenColour);
+    userClickedPattern.push(randomChosenColour)
+    console.log(gamePattern[gamePattern.length - 1]);
 
     switch (randomChosenColour) {
         case "blue":
@@ -62,14 +67,28 @@ function animationButton(pressedKey) {
 }
 
 $(".btn").click(function(){ //handler funxtion store id of clicked button
+    nextSequence();
     var userChosenColour = $(this).attr('id');
-    userClickedPattern.push(userChosenColour)
+    userClickedPattern.push(userChosenColour);
     var lastElement = userClickedPattern[userClickedPattern.length - 1];
     playSound(lastElement);
-    console.log(userClickedPattern);
+    checkAnswer(userClickedPattern.length - 1) /// push index of the last answer the last answer.
+
+    console.log(gamePattern[gamePattern.length - 1]); //ok gamepattern
+   console.log(userClickedPattern.length - 1); //ok userclicked pattern
 });
 
 function playSound(buttonColor) {
     var audio = new Audio("sounds/" + buttonColor + ".mp3");
     audio.play();
+}
+
+function checkAnswer(currentLevel){ //check if the most recent user answer is the same as the game pattern
+    var gamePattern1 = gamePattern[gamePattern.length-1];
+    var userClickedPattern1 = userClickedPattern[currentLevel];
+    if(userClickedPattern1 == gamePattern1){
+        console.log("success");
+    } else {
+        console.log("fail");
+    }
 }
